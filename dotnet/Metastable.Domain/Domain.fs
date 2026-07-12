adae89969703465e76f8bdf7de2f76903bde2b9d
Daniel Molinero Lucas <danielmolinero111@gmail.com>
2026-07-12 14:47:23 +0200

    Expose stable F# domain self-test boundary

--- dotnet/Metastable.Domain/Domain.fs
+++ dotnet/Metastable.Domain/Domain.fs
@@ -18,10 +18,19 @@ type ExperimentRequest = {
 }
 
 module Validation =
-    let validateRequest request =
+    let validateRequest (request: ExperimentRequest) =
         if String.IsNullOrWhiteSpace request.SchemaVersion then
             Error "schema_version is required"
         elif String.IsNullOrWhiteSpace request.ExperimentId then
             Error "experiment_id is required"
         else
             Ok request
+
+    let selfTest () =
+        let request = {
+            SchemaVersion = "1.0.0"
+            RequestId = Guid.Parse "11111111-1111-1111-1111-111111111111"
+            ExperimentId = "self-test"
+            SubmittedAtUtc = DateTimeOffset.UnixEpoch
+        }
+        validateRequest request |> Result.isOk
